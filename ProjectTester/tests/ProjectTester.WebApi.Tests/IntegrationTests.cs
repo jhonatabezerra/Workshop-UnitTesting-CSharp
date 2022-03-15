a135784e1986754774939c07a7a67619ddd7d41b
jhonata.empresa <jhonata.empresa@gmail.com>
2022-03-15 20:20:23 -0300

    Initial commit - Projects created

--- ProjectTester/tests/ProjectTester.WebApi.Tests/IntegrationTests.cs
+++ ProjectTester/tests/ProjectTester.WebApi.Tests/IntegrationTests.cs
@@ -1,37 +1,37 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace ProjectTester.WebApi.Tests
 {
-    public class IntegrationTests : IClassFixture<WebApplicationFactory<WebApplication>>
+    public class IntegrationTests : WebApplicationFactory<Program>
     {
-        private readonly WebApplicationFactory<WebApplication> _factory;
-
-        public IntegrationTests(WebApplicationFactory<WebApplication> factory)
+        protected override IHost CreateHost(IHostBuilder builder)
         {
-            _factory = factory;
+            // shared extra set up goes here
+            return base.CreateHost(builder);
+
+            
         }
 
-        [Theory]
-        [InlineData("/")]
-        [InlineData("/Index")]
-        [InlineData("/About")]
-        [InlineData("/Privacy")]
-        [InlineData("/Contact")]
-        public async Task Get_Endpoints_ShouldReturnSuccessAndCorrectContentType(string url)
+        [Fact]
+        public async Task Test()
         {
-            // Arrange
-            var client = _factory.CreateClient();
-
-            // Act
-            var response = await client.GetAsync(url);
+            var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // set up servises
+                });
+            });
+            var client = application.CreateClient();
+            var response = await client.GetStringAsync("/api/WeatherForecast");
 
             // Assert
-            response.EnsureSuccessStatusCode();
-            Assert.Equal("text/html; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
         }
     }
 }
